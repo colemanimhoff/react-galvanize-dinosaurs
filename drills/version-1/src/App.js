ddc1786b33322c9ee68741e9cce892ea169f437f
Coleman Imhoff <colemanimhoff@gmail.com>
2018-04-23 08:56:06 -0600

    moved all components from existing repo

--- drills/version-1/src/App.js
+++ drills/version-1/src/App.js
@@ -1,21 +1,44 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import JobList from './components/JobList'
+import JobForm from './components/JobForm'
 
-class App extends Component {
+class App extends React.Component {
+
+    constructor (props) {
+    super (props)
+    this.state = {
+      listings: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('./listings.json')
+    .then(response => response.json())
+    .then(response => {
+      this.setState({ listings: response })
+    })
+  }
+
+  submitJobListing = (listing) => {
+    this.setState({ listings: this.state.listings.concat(listing).reverse() })
+  }
+    
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header />
+        <main>
+          <JobList listings={this.state.listings} />
+          <JobForm submitJobListing={this.submitJobListing}/>
+        </main>
+        <Footer />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
